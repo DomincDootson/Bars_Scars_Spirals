3617c580b2b6e7263d80d7962de0add5b3b4d837
Dominic Dootson <dominic.dootson@physics.ox.ac.uk>
2021-01-01 12:38:28 +0000

    Add some functions to Bar2D.h

--- Bar2D/Bar2D.h
+++ Bar2D/Bar2D.h
@@ -6,22 +6,21 @@
 #include <complex>
 #include <iostream>
 
-
-
-
-// Pass it the basis functions ?? 
+#include <../Potential_Density_Pair_Classes/PotentialDensityPairContainer.h>
 
 class Bar2D
 {
 public:
-	Bar2D(const basisFunctionContainer &basisFunctions) :
-	m_theta, m_omega, m_alpha{0}, m_momentOfInertia{momentOfInertia(basisFunctions)}
+	Bar2D(const Eigen::VectorXcd expansionCoeff, const basisFunctionContainer &basisFunctions) :
+	m_theta, m_omega, m_alpha{0},
+	m_expansionCoeff{expansionCoeff};
+	m_momentOfInertia{momentOfInertia(basisFunctions)}
 	m_scriptE{basisFunction.getScriptE()};
 	{}
 	~Bar2D() {} 
 
 	double torque(const Eigen::VectorXcd &diskCoeff) const;
-	double momentOfInertia(basisFunctionContainer &basisFunctions) const; // We porbably need to pass it the basis functions
+	double momentOfInertia(const basisFunctionContainer &basisFunctions) const;
 
 	void drift(const double timeStep);
 	void kick(const double timeStep);
@@ -30,11 +29,14 @@ public:
 
 	Eigen::MatrixXd barCoeff() const {return m_expansionCoeff;} // Maybe we could just overload = sign? 
 private: 
-	double m_theta, m_omega, m_alpha, m_momentOfInertia;
+	double m_theta, m_omega, m_alpha; // Note that alpha = Torque/MOI.
+	Eigen::VectorXcd m_expansionCoeff;
+
+	const double m_momentOfInertia;  
 	const int m_fourierHarmonic;  
 	const Eigen::MatrixXd m_scriptE; 
 
-	Eigen::VectorXcd m_expansionCoeff
+	
 
 	Eigen::ArrayXXd rSquaredArray(const int nGrid, const double rMax) const;
 };
@@ -45,18 +47,18 @@ void Bar2D::saveBarEvolution(ofstream &outPut) const {
 
 double Bar2D::torque(const Eigen::VectorXcd &diskCoeff) const // Torque = il * (A.scriptE.conj(B) - b.scriptE.conj(A))
 {
-	complex<double>    firstTerm{ m_expansionCoeff.dot(m_scriptE *  diskCoeff.conjugate()) };
-	complex<double> secondTerm{ m_diskCoeff.dot(m_scriptE *  m_expansionCoeff.conjugate()) }; 
-	complex<double> unitComplex(0,1);
-	return unitComplex * m_fourierHarmonic (firstTerm - secondTerm); 
+	std::complex<double>    firstTerm{ m_expansionCoeff.dot(m_scriptE *  diskCoeff.conjugate()) };
+	std::complex<double> secondTerm{ m_diskCoeff.dot(m_scriptE *  m_expansionCoeff.conjugate()) }; 
+	std::complex<double> unitComplex(0,1);
+	return std::real(unitComplex * m_fourierHarmonic (firstTerm - secondTerm)); 
 
 }
 
 Eigen::ArrayXXd Bar2D::rSquaredArray(const int nGrid, const double rMax) const{ // Should all be fine, but a quick check won't hurt! 
 	Eigen::ArrayXXd rSquaredArray{Eigen::ArrayXXd::Zero(nGrid, nGrid)};
 	double x{}, y{}, rSquared{}, spacing{2*rMax/((double) nGrid-1)}, centre{0.5*(nGrid-1)};
-	for (int i = 0; i< rSquaredArray.rows(); ++i){
-		for (int j = 0; j<rSquaredArray.cols();++j){
+	for (int i = 0; i < rSquaredArray.rows(); ++i){
+		for (int j = 0; j < rSquaredArray.cols(); ++j){
 			x = spacing * (i - centre); y = spacing * (j - centre); rSquared = x*x +y*y;
 			rSquaredArray(i,j) = rSquared;
 		}
@@ -65,7 +67,7 @@ Eigen::ArrayXXd Bar2D::rSquaredArray(const int nGrid, const double rMax) const{ 
 }
 
 
-double momentOfInertia(basisFunctionContainer &basisFunctions) const { // Ditto with this, a check wouldn't hurt 
+double Bar2D::momentOfInertia(basisFunctionContainer &basisFunctions) const { // Ditto with this, a check wouldn't hurt 
 	int nGrid{801}; // Parameters that set the size of the array. 
 	double rMax{20};
 
@@ -76,14 +78,18 @@ double momentOfInertia(basisFunctionContainer &basisFunctions) const { // Ditto 
 }
 
 void Bar2D::drift(const double timeStep){
-	complex<double> unitComplex(0,1);
+	std::complex<double> unitComplex(0,1);
 	double deltaTheta{m_omega*timeStep + 0.5 * m_alpha * timeStep * timeStep}; 
 	
 	m_expansionCoeff = exp(-unitComplex * deltaTheta * m_fourierHarmonic);
 	m_theta += deltaTheta; 
 }
 
-void Bar2D::kick(const double timeStep);
+void Bar2D::kick(const double timeStep, const Eigen::VectorXcd &diskCoeff){
+	double oldAlpha = m_alpha;
+	m_alpha = torque(diskCoeff)/m_momentOfInertia;
+	return 0.5 * (oldAlpha + m_alpha) * timeStep; 
+}
 
 
 #endif
